66b5e56b40f8903b2b21acb5650d3d0f13707ab7
Dario Seminara <dario@bandofcoders.com>
2011-12-07 16:59:19 -0300

    changed full host to use heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ print "*"*60,"\n"
 
 require "openid/store/filesystem"
 require "omniauth_patch" # located at lib dir
-OmniAuth.config.full_host = "http://localhost:3000"
+OmniAuth.config.full_host = "http://warm-moon-7086.heroku.com/"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 #  provider :google, 'domain.com', 'oauth_secret', :scope => 'https://mail.google.com/mail/feed/atom/'
