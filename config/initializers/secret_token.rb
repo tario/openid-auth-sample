d536757a770cda3eb46f477e91ed9b5054d3e73b
Dario Seminara <dario@bandofcoders.com>
2011-12-07 11:53:00 -0300

    rails basic application made from scartch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthDemo2::Application.config.secret_token = '7879b2d12edf0a5bab8b2d1c4a08ab9d2b13d8daa49d868c7a0d26747b58c033c8fa977b669b7c7c2e782978e1e5beafab3e4e043fe0e4f548a7fcaa7bf10914'
