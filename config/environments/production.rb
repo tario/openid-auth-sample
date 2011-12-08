395b43d92ee11d57f642848cafb71a74ec1fba2d
Dario <darios3@gmail.com>
2011-12-08 11:58:22 -0300

    removed cache config from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,6 @@ OmniauthDemo2::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.cache_store = :mem_cache_store, Memcached::Rails.new
+#  config.cache_store = :mem_cache_store, Memcached::Rails.new
 end
 
